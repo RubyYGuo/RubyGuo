6a5ec8941a7996ae3b83247ba87bb002d997150c
RubyGuo <57688562+RubyYGuo@users.noreply.github.com>
2026-05-12 18:22:41 -0400

    Modify run_capuchin_suite.sh for new environment and tests
    Updated the script to use a different conda environment and added hardware testing phase.

--- stgt_scripts/run_capuchin_suite.sh
+++ stgt_scripts/run_capuchin_suite.sh
@@ -1,28 +1,16 @@
 #!/bin/bash
 
-# Activate conda and run YOLO
-#source ~/miniconda3/etc/profile.d/conda.sh
-#conda activate capuchinyolov5
-#python /home/capuchin/yolo5model/yolov5/capuchin_recorder_display_fpsfix_headless.py &
-#PID1=$!
-
-# Use system Python for GPIO reward script
-# sudo /usr/bin/python3 /home/capuchin/Desktop/stgt_task.py
-
-# Kill YOLO process if still running
-#kill $PID1
-
-
-
-
-# USE THIS: 
-#!/bin/bash
-
 # Load conda
 source "$HOME/miniconda3/etc/profile.d/conda.sh"
 
-# Activate environment 
-conda activate capuchinyolov5
+# Activate environment
+conda activate capuchinyolo26
 
 # Run integrated YOLO + STGT controller
-python3 -i /home/capuchin/yolo5model/yolov5/stgt_scripts/stgt_master_script.py --csi 0 1
+cd /home/capuchin/Desktop/stgt_scripts
+
+# 1. Run the hardware testing phase
+python3 stgt_io_test.py
+
+# 2. Proceed to the master script upon test completion
+python3 -i stgt_master_script.py --csi 0 1
